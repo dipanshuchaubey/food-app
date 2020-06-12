3cc4c81c3bf9fe9619141ef6be8d7ab10ff9cea6
Dipanshu Chaubey <dipanshuchaubey@gmail.com>
2020-06-13 02:15:33 +0530

    Search businesses using Yelp API

--- src/screen/Search.js
+++ src/screen/Search.js
@@ -1,9 +1,28 @@
 import React, {useState} from 'react';
-import {View, StatusBar, StyleSheet} from 'react-native';
+import {View, Text, StatusBar, StyleSheet} from 'react-native';
 import SearchBar from '../components/SearchBar';
+import yelp from '../api/yelp';
 
 const SearchScreen = () => {
   const [searchText, setSearchText] = useState('');
+  const [result, setResult] = useState([]);
+  const [error, setError] = useState('');
+
+  const searchApi = async () => {
+    try {
+      const response = await yelp.get('/search', {
+        params: {
+          term: searchText,
+          location: 'san jose',
+          limit: 50,
+        },
+      });
+
+      setResult(response.data.businesses);
+    } catch (err) {
+      setError('Something went wrong, Please try again!');
+    }
+  };
 
   return (
     <View>
@@ -12,8 +31,12 @@ const SearchScreen = () => {
       <SearchBar
         searchText={searchText}
         setSearchText={setSearchText}
-        onSubmit={() => console.log('Submitted')}
+        onSubmit={() => searchApi()}
       />
+
+      {error ? <Text>{error}</Text> : null}
+
+      <Text>Found total {result.length} results</Text>
     </View>
   );
 };
